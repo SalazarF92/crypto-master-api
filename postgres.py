201b1af1c10b2aa1a2f4735ffc698b1b5419d92a
Flavio Salazar <flavio.campinas92@gmail.com>
2023-01-03 20:40:01 -0300

    fix: envs

--- postgres.py
+++ postgres.py
@@ -9,10 +9,10 @@ class Connection:
         try:
             self.conn = psycopg2.connect(
                 user=os.getenv("DB_USER"),
-                password=os.getenv("DB_PASSWORD"),
-                host=os.getenv("DB_HOST"),
-                port=os.getenv("DB_PORT"),
-                database=os.getenv("DB_NAME"),
+                password=os.getenv("PGPASSWORD"),
+                host=os.getenv("PGHOST"),
+                port=os.getenv("PGPORT"),
+                database=os.getenv("PGDATABASE"),
             )
             self.cursor = self.conn.cursor()
 
